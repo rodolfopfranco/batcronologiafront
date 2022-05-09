58910255d795069ba5be4eaadc725d73eba33d60
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2022-05-09 15:37:12 -0300

    added material with ng add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TesteComponent } from './teste/teste.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { TopMenuComponent } from './top-menu/top-menu.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
