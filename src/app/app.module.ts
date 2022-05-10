1554ace439946dbe91428d06f194bf799dcbf7c8
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2022-05-10 11:32:32 -0300

    Changed top menu appearance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TesteComponent } from './teste/teste.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
+
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card'; 
+import {MatIconModule} from '@angular/material/icon'; 
 
 @NgModule({
   declarations: [
@@ -21,7 +24,9 @@ import { MatTabsModule } from '@angular/material/tabs';
     AppRoutingModule,
     RouterModule,
     NoopAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
