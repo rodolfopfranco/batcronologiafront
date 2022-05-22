c5f00869da21e83c95d244f6ff857d624250c5ab
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2022-05-22 16:18:00 -0300

    Added BookChrono listing to chronology page (ngfor with argument parsing to component creation)

--- src/app/view/cronologia/cronologia.component.ts
+++ src/app/view/cronologia/cronologia.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { observable } from 'rxjs';
+import { BookChrono } from 'src/app/shared/model/book-chrono.model';
+import { ResponsePageable } from 'src/app/shared/model/response-pageable.model';
+import { BookChronoService } from 'src/app/shared/service/book-chrono.service';
+import { ComicCardComponent } from './comic-card/comic-card.component';
 
 @Component({
   selector: 'app-cronologia',
@@ -7,9 +12,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CronologiaComponent implements OnInit {
 
-  constructor() { }
+  bookChronoList: BookChrono[];
+
+  constructor(private bookChronoService: BookChronoService) { }
 
   ngOnInit(): void {
+    this.bookChronoService.getBookchronoList().subscribe(
+      (observable:ResponsePageable) =>{
+        this.bookChronoList = observable.content;
+      }
+    )
   }
 
 }
