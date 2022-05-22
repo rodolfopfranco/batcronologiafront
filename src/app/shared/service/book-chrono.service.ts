9b563395e46bca7e2995adea5fb39b51db136f08
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2022-05-22 16:18:08 -0300

    Added BookChrono listing to chronology page (ngfor with argument parsing to component creation)

--- src/app/shared/service/book-chrono.service.ts
+++ src/app/shared/service/book-chrono.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ResponsePageable } from '../model/response-pageable.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookChronoService {
 
-  constructor() { }
+  apiUrl = 'http://localhost:8080/v1/bookChronos'
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':'application/json'
+    })
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getBookchronoList(): Observable<ResponsePageable>{
+    return this.httpClient.get<ResponsePageable>(this.apiUrl);
+  }
 }
